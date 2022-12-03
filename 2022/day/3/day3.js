14a9d454078f746550fbc6c1c7a3e7314b5b3395
simplehack <shift.insert+simplehack@gmail.com>
2022-12-03 00:03:33 -0800

    bye index

--- 2022/day/3/day3.js
+++ 2022/day/3/day3.js
@@ -4,7 +4,7 @@ const sum = (arr) => arr.reduce((sum, val) => sum + val);
 const priorities = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const getPriority = (item) =>  priorities.indexOf(item) + 1;
 
-const sum_part_1 = sum(input.map((sack, index) => {
+const sum_part_1 = sum(input.map((sack) => {
     let [c1, c2] = [[...sack].slice(0, sack.length/2), [...sack].slice(sack.length/2)];
     return getPriority(c1.find((x) => c2.includes(x)));
 }));
@@ -19,4 +19,5 @@ for (let x = 0; x <= input.length; x++) {
 const sum_part_2 = sum(groups.map((group) => {
     return getPriority(group[0].find((x) => group[1].includes(x) && group[2].includes(x)));
 }));
+
 console.log([sum_part_1, sum_part_2]);
--- 2022/day/3/part1.js
+++ 2022/day/3/part1.js
@@ -4,8 +4,9 @@ const sum = (arr) => arr.reduce((sum, val) => sum + val);
 const priorities = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const getPriority = (item) =>  priorities.indexOf(item) + 1;
 
-const sum_priorities = sum(input.map((sack, index) => {
+const sum_priorities = sum(input.map((sack) => {
     let [c1, c2] = [[...sack].slice(0, sack.length/2), [...sack].slice(sack.length/2)];
     return getPriority(c1.find((x) => c2.includes(x)));
 }));
+
 console.log(sum_priorities);
