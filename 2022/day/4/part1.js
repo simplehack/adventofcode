df6d0adcfb4b23732d25e768a5d2ca39c48e7015
simplehack <shift.insert+simplehack@gmail.com>
2022-12-03 22:46:38 -0800

    day 4

---
+++ 2022/day/4/part1.js
@@ -0,0 +1,12 @@
+const { input } = require('./input');
+const overlapping_jobs = input.filter((job) => {
+    const [x, y] = [job[0].split('-').map(Number), job[1].split('-').map(Number)];
+    return (x[0] <= y[0] && y[1] <= x[1]) || (y[0] <= x[0] && x[1] <= y[1]);
+});
+console.log(overlapping_jobs.length);
+
+const any_overlaps = input.filter((job) => {
+    const [x, y] = [job[0].split('-').map(Number), job[1].split('-').map(Number)];
+    return (y[0] >= x[0] && y[0] <= x[1]) || (x[0] >= y[0] && x[0] <= y[1]);
+})
+console.log(any_overlaps.length)
