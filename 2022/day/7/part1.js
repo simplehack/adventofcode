6744066d7e7ff2738b1210996256b38ee100f26c
simplehack <shift.insert+simplehack@gmail.com>
2022-12-07 22:43:04 -0800

    solution

--- 2022/day/7/part1.js
+++ 2022/day/7/part1.js
@@ -2,15 +2,22 @@ const { allowedNodeEnvironmentFlags } = require('process');
 const { input } = require('./input');
 const sum = (arr) => arr.reduce((sum, val) => sum + val);
 
-
+const updateSize = (parent, size) => {
+    parent._size = parent._size + size;
+    if (typeof parent._parent !== 'undefined') updateSize(parent._parent, size);
+}
+const getPath = (dir, path = '') => {
+    if (typeof dir._parent === 'undefined') return path;
+    path = `${dir._name}/${path}`;
+    if (dir && dir._parent) {
+        return getPath(dir._parent, path)
+    }
+    return path;
+}
 const process_commands = (input) => {
     let output = {};
     let dir = output;
     let d;
-    const updateSize = (parent, size) => {
-        parent._size = parent._size + size;
-        if (typeof parent._parent !== 'undefined') updateSize(parent._parent, parent._size);
-    }
     input.forEach((x) => {
         switch (true) {
             case /^\$ cd/.test(x):
@@ -18,14 +25,13 @@ const process_commands = (input) => {
                 if (d == '..') {
                     dir = dir._parent;
                 } else {
+                    d = getPath(dir) + d;
                     if (d === '/') dir[d] = {_size: 0, _name: d}
                     dir = dir[d];
                 }
                 break;
-            case /^$ ls/.test(x):
-                break;
             case (/^dir/).test(x):
-                d = x.split(' ')[1]
+                d = getPath(dir) + x.split(' ')[1];
                 dir[d] = {_parent: dir, _size: 0, _name: d}
                 break;
             case (/^\d+/).test(x):
@@ -33,30 +39,37 @@ const process_commands = (input) => {
                 dir[f[1]] = parseInt(f[0], 10);
                 updateSize(dir, dir[f[1]]);
                 break;
-            default:
         }
     });
     return output;
 }
 
-const getDirectoriesBelowSize = (input, maxsize) => {
-    const testAllDirs = (d, dirs) => {        
-        Object.keys(d).forEach( k => {
+const getDirectoriesBySize = (input, fn) => {
+    const testAll = (d, matches) => {        
+        Object.keys(d).forEach((k) => {
             if (typeof d[k] === 'object' && k != '_parent') {
-                return testAllDirs(d[k], dirs)
+                return testAll(d[k], matches)
             }
-            if (k === '_size' && d[k] <= maxsize) {
-                dirs.push({[d._name]:d[k]})
+            if (k === '_size' && fn.call(this, d[k])) {
+                matches.push({[d._name]:d[k]})
             }
-        })
-        return dirs
+        });
+        return matches;
     }
-    return testAllDirs(input, []);
+    return testAll(input, []);
 }
 
-
-const getSumOfDirectoriesBelowSize = (input, size) => {
-    return sum(getDirectoriesBelowSize(process_commands(input), size).map(o => Object.values(o)[0]));
+const getSumOfDirectoriesBelowSize = (fs, size) => {
+    return getDirectoriesBySize(fs, (s) => s <= size);
 }
 
-console.log(getSumOfDirectoriesBelowSize(input, 100000));
+const getMinimumNecessaryToDelete = (fs, capacity, space_needed) => {
+    const available_space = capacity - fs['/']._size;
+    const minsize = space_needed - available_space;
+    const dirs = getDirectoriesBySize(fs, (s) => s >= minsize);
+    return dirs.sort((a,b) => a - b).pop()
+}
+
+const fs = process_commands(input);
+console.log('part 1', sum(getSumOfDirectoriesBelowSize(fs, 100000).map(o => Object.values(o)[0])));
+console.log('part 2', getMinimumNecessaryToDelete(fs, 70000000, 30000000));
